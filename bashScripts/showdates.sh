7a723e85512fb44d9ae04baa76b3c2a3d56454fb
danilomo <dmo4@cin.ufpe.br>
2017-04-01 09:39:15 +0200

    Removing unnecessary files

--- bashScripts/showdates.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#date +"%T.%N"
-#sshpass -p '12345' ssh teste@192.168.122.61 "date +\"%T.%N\""
-#sshpass -p '12345' ssh teste@192.168.122.34 "date +\"%T.%N\""
-
-date
-sshpass -p '12345' ssh teste@192.168.122.61 "date"
-sshpass -p '12345' ssh teste@192.168.122.34 "date"
