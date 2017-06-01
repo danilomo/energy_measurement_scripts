142721988eb02c7b0937c05b12f68c1b291346c8
danilomo <dmo4@cin.ufpe.br>
2017-06-01 15:23:12 +0200

    Logic for using cpulimit on VM process.

--- start_experiment.sh
+++ start_experiment.sh
@@ -7,7 +7,7 @@ function getVMPid {
 
 # Get the PID of the qemu process by domain name
 function getVCPUID {
-	sudo grep pid /var/run/libvirt/qemu/$1.xml | grep vcpu | grep -Eo '[0-9]*'
+	sudo grep pid /var/run/libvirt/qemu/$1.xml | grep vcpu | grep -Eo "pid='[0-9]*'" | grep -Eo "[0-9]*"	
 }
 
 # Sleep until a given date
@@ -34,6 +34,7 @@ baseTime=$(jq '.baseTime' experiment_config.json | sed -e 's/^"//' -e 's/"$//' )
 experimentDuration=$(jq '.experimentDuration' experiment_config.json)
 samplingInterval=$(jq '.samplingInterval' experiment_config.json)
 provider=$(jq '.provider' ./configFiles/config.json | sed -e 's/^"//' -e 's/"$//')
+limit=$(jq '.cpulimit' ./configFiles/config.json | sed -e 's/^"//' -e 's/"$//'	)
 
 
 # Start monitor processes for CPU, network, and IO of VMs
@@ -41,6 +42,7 @@ echo "Starting VM monitors..."
 while read p; do
 	pid=$(getVMPid $p)
 	vcpupid=$(getVCPUID $p)
+	
 	int=$(python ./pythonScripts/find_interface_name.py "./configFiles/provider_config.json" $provider $p)
 	./monitorScripts/monitor_cpu_process.sh "$baseTime" $experimentDuration $samplingInterval $vcpupid $p &
 	./monitorScripts/monitor_memory_process.sh "$baseTime" $experimentDuration $samplingInterval $pid $p &	
@@ -63,15 +65,17 @@ echo "Starting benchmarks..."
 python ./pythonScripts/start_experiment.py "./configFiles/provider_config.json" "./configFiles/config.json"
 
 
-# Blocks until experiment ends
+# put limit on VM process, if field set on the JSON file
+if [ "$limit" != "null" ]; then
+	while read p; do
+		pid=$(getVMPid $p)
+		sudo timeout $experimentDuration cpulimit -p $pid -l $limit &			
+	done < instances.txt
+fi
+
 wait $p
 
 echo "Experiment finished!"
-
-#echo "Stopping virtual machines"
-#while read p; do
-#	virsh  shutdown $p --mode acpi
-#done < instances.txt
 
 # Cleaning up files
 rm instances.txt
